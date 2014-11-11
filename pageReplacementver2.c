6333d87ad553bd36bd69abb2f85d983f541a6b18
joshuafrancisroman <joshuafrancisroman@gbiz.com>
2014-11-11 17:34:09 +0800

    revised

--- pageReplacementver2.c
+++ pageReplacementver2.c
@@ -1,104 +1,70 @@
 
 #include <stdio.h>
 #include <stdlib.h>//contains malloc and free
-
-//read THIS
-//need mong malagay yung input string into an array for passing. Also, need ko rin malaman yung number of arguments or arr.length.
-
-int FIFOalg(int input[], int n, int inputSize);//input is user input values delimited by spaces. N is size of array per output
+int FIFOalg(int input[], int n, int inputSize);//input is user input values delimited by spaces. N is size of array per output. input size is number of pages
 int LRUalg(int input[], int n, int inputSize);
 int OPTalg(int input[], int n, int inputSize);
 int isFull(int* LRU, int n);
 void printFrames(int* LRU, int n);
 void pageReplace();
-int main()
-{
-	char input;
-	char *i;
-	do{
-		pageReplace();
-		gets(&i);
-		printf("\n\nDo you want to input another Reference String?[y/n]: ");
-		scanf("%c", &input);
-		gets(&i);
-		if(input != 'n' || input != 'N' || input != 'y' || input != 'Y'){
-			do{
-				printf("\n\nDo you want to input another Reference String?[y/n]: ");
-				scanf("%c", &input);
-				gets(&i);
-				if(input == 'n' || input == 'N' || input == 'y' || input == 'Y'){
-					break;
-				}
-			}while(1);
-		}
-		if(input == 'n' || input == 'N'){
-			break;
-		}
-		if(input == 'y' || input == 'Y'){
-			continue;
-		}
-	}while(1);
-    return 0;
-}
 
-void pageReplace(){
-    int n;
-    char *p;
-    char str[50];
-    int input[50];
-	int i = 0, j;//i is input size
-	float output;
-	printf("Input Reference String: ");
-	gets(str);
-	//tokenize string and converts into integer array
-	p = strtok(str, " ");
-	input[i++] = atoi(p);
-	do{
-		p = strtok(NULL, " ");
-		if(p != NULL)
-			input[i++] = atoi(p);
-	}while(p);
-
-    printf("Input size of page frame: ");
-    scanf("%d", &n);
-    output = FIFOalg(input, n, i);
-    printf("Number of page faults:%d\n", (int)output);
-    printf("Percentage: %.2f\n", ((output/i)*100));
-    
-    output = OPTalg(input, n, i);
-    printf("Number of page faults:%d\n", (int)output);
-	printf("Percentage: %.2f\n", ((output/i)*100));
-	
-	output = LRUalg(input, n, i);
-    printf("Number of page faults:%d\n", (int)output);
-	printf("Percentage: %.2f\n", ((output/i)*100));
-	 
-    free(p);
+int main(){
+    char choice; //all user char inputs
+    do{
+        system("cls"); 
+        int n,p,output,y;//n = number of pages. p = framesize. output = pageFaults, y = loop ctr
+    	printf("Input number of requested page: ");
+    	scanf("%d",&n);
+        printf("Input number of frames: ");
+        scanf("%d", &p);
+        int *input=malloc(n*sizeof(int));//dynamic array. n elements
+        for(y=0; y<n; y++){
+                printf("RP%d:",y+1);
+                scanf("%d",&input[y]); 
+        }
+        printf("\tPage Replacement Algorithm\n");
+        printf("[A] First-in First-out (FIFO)\n");
+        printf("[B] Optimal (OPT)\n");
+        printf("[C] Least Recently Used (LRU)\n");
+        printf("[D] Exit\n");
+        printf("Enter Choice:");
+        scanf("%s",&choice);
+        switch(choice){
+            case 'A':
+            case 'a':output = FIFOalg(input, p, n);break;
+            case 'B':
+            case 'b':output = OPTalg(input, p, n);break;
+            case 'C':
+            case 'c':output = LRUalg(input, p, n);break;
+            case 'd':
+            case 'D':return 0;
+            default:return 0;	
+        }
+        printf("Number of requested page= %d\n",n);
+        printf("Number of frames= %d\n",p);
+        printf("Page fault: %d\n", output);
+       	printf("Page fault percentage: %.2f\n", ((float)output/n)*100);
+        free(input);
+        printf("Input again[y/n]:");
+        scanf("%s",&choice);
+    }while(choice=='y' || choice=='Y');
     return 0;
 }
 
 //FIFO algo, check if inputted value exists in array, if yes, dont move cursor and get next val
 //else if not, put in cursor pos then next val. and also print the whole array.
-int FIFOalg(int input[], int n, int inputSize){//'input' array is user input delimited by spaces.
-    int pageFault = 0;
-    int cursor = 0;
+int FIFOalg(int input[], int n, int inputSize){
+    int x,y,check,pageFault = 0,cursor = 0;
     int exists = 0;//0 = false 1 = true
     printf("\nFIFO page replacement algorithm:\n");
     int *FIFO; //this is a pointer to the stack (dynamic allocation using stacks as arrays
     FIFO = malloc( n * sizeof(int) );//N = number of memory locations in the stack, sizeOf = how big each location is. Returns a pointer
-   
-    if(FIFO==0 || FIFO==NULL)
-    {
+    if(FIFO==0 || FIFO==NULL){//no allocatable memory
         printf("EMPTY");
         return -1;
-    }
-    int x;
-    int check;
-    int y;        
-    for(x = 0; x < n; x++)
+    } 
+    for(x = 0; x < n; x++)//initializing to pseudo-null
         FIFO[x] = -40000;//just using -40000 as a reference to null for all the values in the array
-    
-    
     for (x = 0; x < inputSize; x++){
         for (check = 0; check < n; check++){//this loop checks if the value is existing in the array
             if (input[x] == FIFO[check]){
@@ -107,7 +73,6 @@ int FIFOalg(int input[], int n, int inputSize){//'input' array is user input del
             }
             exists = 0;
         }
-        
         if (!exists){//if values does not exist in array, put in current cursor pos then output array
             pageFault++;
             FIFO[cursor++] = input[x];
@@ -120,7 +85,6 @@ int FIFOalg(int input[], int n, int inputSize){//'input' array is user input del
             }
             printf("\n");
         }
-        
         if (cursor == n)
            cursor = 0;//circular array
     }
@@ -137,14 +101,11 @@ int LRUalg(int input[], int n, int inputSize){
            printf("Cannot allocate memory");
            return -1;
     }
-    
     for(x = 0 ; x < n; x++){//initialize block of memory
           LRU[x] = -40000;
           prec[x] = -40000;    
     }
-    
     printf("\nLRU page replacement algorithm:\n");
-    
     for(x = 0 ; x < inputSize; x++){
           for(check = 0; check < n ; check++){//checks if value already exists
             if( input[x] == LRU[check]){
@@ -153,8 +114,7 @@ int LRUalg(int input[], int n, int inputSize){
                 break;
             }
             exists = 0;          
-          }
-          
+          }      
           if(!exists && isFull(LRU,n)==0){
              pageFault++;
              LRU[cursor] = input[x];
@@ -168,7 +128,6 @@ int LRUalg(int input[], int n, int inputSize){
              }
              printf("\n");
           }
-          
           else if(!exists && isFull(LRU,n)==1){
                pageFault++;     
                int min = 40000,minL;
@@ -177,7 +136,6 @@ int LRUalg(int input[], int n, int inputSize){
                            min = prec[y];
                            minL = y;       
                     }
-               
                cursor = minL; //insertion point
                LRU[cursor] = input[x];
                prec[cursor] = precVal++;
@@ -191,14 +149,12 @@ int LRUalg(int input[], int n, int inputSize){
                printf("\n");          
           }    
     }
-    
-    
     free(LRU);
     free(prec);
     return pageFault;
 }
 
-int ifExist(int* frames, int n, int key){
+int ifExist(int* frames, int n, int key){//check if value is existing in frame
 	int i;
 	for(i = 0; i < n; i++){
 		if(frames[i] == key){
@@ -208,7 +164,7 @@ int ifExist(int* frames, int n, int key){
 	return 0;
 }
 
-int isFull(int* LRU, int n){
+int isFull(int* LRU, int n){//check if frame is full
     int full = 0, x;
     for( x = 0; x < n; x++){
          if(LRU[x]==-40000){
@@ -220,7 +176,7 @@ int isFull(int* LRU, int n){
     return full;  
 }
 
-void printFrames(int* LRU, int n){
+void printFrames(int* LRU, int n){//prints the frames
     int full = 0, x;
     for( x = 0; x < n; x++){
     	 printf("|");
@@ -283,7 +239,7 @@ int OPTalg(int input[], int n, int inputSize){
 	}
 	return pageFault;
 }
-int getOptIndex(int *string, int current_pointer, int size_ref, int *frames, int frame_size ){
+int getOptIndex(int *string, int current_pointer, int size_ref, int *frames, int frame_size ){//finds best place for insertion point
 	int x, y, isWeightComputed = 1, index = current_pointer % frame_size;
 	int indexScore[frame_size];//weights which is least used in frames
 	for(x = 0; x < frame_size; x++){
